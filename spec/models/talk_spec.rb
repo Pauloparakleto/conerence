51edf1f0fd2b106f320e4b10ec9b11e699b7fdc4
paulo-felipe <alparakleto@gmail.com>
2022-10-22 16:53:04 -0300

    test model/talk_spec.rb: add when update and refactor when create scenario

--- spec/models/talk_spec.rb
+++ spec/models/talk_spec.rb
@@ -3,17 +3,34 @@ require 'rails_helper'
 RSpec.describe Talk, type: :model do
   let(:current_time) { Time.now }
 
-  describe 'create' do
-    context 'when it is valid' do
-      let(:talk) { FactoryBot.create :talk }
+  context 'when create' do
+    let(:talk) { FactoryBot.create :talk }
 
-      it 'has name' do
-        expect(talk.name).to eq('MyString')
-      end
+    it 'has name' do
+      expect(talk.name).to eq('MyString')
+    end
 
-      it 'has initial_time true' do
-        expect(talk.initial_time.hour).to eq(9)
-      end
+    it 'has initial_time true' do
+      expect(talk.initial_time.hour).to eq(9)
+    end
+  end
+
+  context 'when update' do
+    let(:talk) { FactoryBot.create :talk }
+    let(:talk_name) { talk.name }
+    let(:talk_initial_time) { talk.initial_time }
+
+    before do
+      talk.update(name: 'AnotherString')
+      talk.update(initial_time: '10:00')
+    end
+
+    it 'updates name' do
+      expect(talk.reload.name).not_to eq('MyString')
+    end
+
+    it 'updates initial_time.hour' do
+      expect(talk.reload.initial_time.hour).not_to eq(9)
     end
   end
 end
