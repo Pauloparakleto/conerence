313c3e6b2c285ef6e6121abf92488d418fb1297f
paulo-felipe <alparakleto@gmail.com>
2022-10-22 20:59:09 -0300

    chore talks_controller.rb: add crud actions

---
+++ app/controllers/talks_controller.rb
@@ -0,0 +1,50 @@
+class TalksController < ApplicationController
+  def index
+    @talks = Talk.all
+  end
+
+  def show
+    @talk = Talk.find(params[:id])
+  end
+
+  def new
+    @talk = Talk.new
+  end
+
+  def create
+    @talk = Talk.new(talk_params)
+    
+    if @talk.save
+      redirect_to @talk
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @talk = Talk.find(params[:id])
+  end
+
+  def update
+    @talk = Talk.find(params[:id])
+
+    if @talk.update(talk_params)
+      redirect_to @talk
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @talk = Talk.find(params[:id])
+    @talk.destroy
+
+    redirect_to talks_path
+  end
+
+  private
+
+    def talk_params
+      params.require(:talk).permit(:name, :initial_time)
+    end
+end
