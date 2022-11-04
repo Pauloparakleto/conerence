b2a75e930d4913fa1aacf2b961186db067ce0141
paulo-felipe <alparakleto@gmail.com>
2022-11-04 19:03:01 -0300

    refactor talsk_creator.rb: use contant to base begining time

--- app/lib/csv/talks_creator.rb
+++ app/lib/csv/talks_creator.rb
@@ -4,6 +4,7 @@ require 'csv'
 module Csv
   class TalksCreator
     attr_accessor :csv_file
+    BASE_INITIAL_TIME = '09:00'
 
     def initialize(csv_file)
       @csv_file = csv_file
@@ -18,7 +19,7 @@ module Csv
         #TODO: check cases when there is commas in name, must be created with commas
         name = csv.join.split(/\w+\z/).first.strip!
 
-        initial_time = talks.length.zero? ? '09:00' : talks.last.initial_time + time_duration.minutes
+        initial_time = talks.length.zero? ? BASE_INITIAL_TIME : talks.last.initial_time + time_duration.minutes
         # FIXME: is there another way to avoid calling joint twice?
         time_duration = csv.join.scan(/\w+$/).join.split(/min/).first.to_i
 
