e5c199d856717c5c0c832acb26589b86cef8c357
paulo-felipe <alparakleto@gmail.com>
2022-11-04 14:19:54 -0300

    refactor spec/lib/csv/talks_creator_spec.rb: move call to a before action when create valids

--- spec/lib/csv/talks_creator_spec.rb
+++ spec/lib/csv/talks_creator_spec.rb
@@ -14,38 +14,32 @@ RSpec.describe Csv::TalksCreator, type: :model do
   describe '#call' do
     let!(:valid_talks_csv) { file_fixture('valid_talks.csv') }
 
-    it 'creates first talk' do
-      valid_talks_creator_initialize.call
+    context 'when create valids' do
+      before { valid_talks_creator_initialize.call }
 
-      expect(Talk.first).to be_truthy
-    end
-
-    it 'has first talk name equal to first csv line without 60min' do
-      valid_talks_creator_initialize.call
-
-      expect(Talk.first.name).to eq('Diminuindo tempo de execução de testes em aplicações Rails enterprise')
-    end
-
-    it 'has talk initial_time.hour equal to 9' do
-      valid_talks_creator_initialize.call
-
-      expect(Talk.first.initial_time.hour).to eq(9)
-    end
-
-    it 'has first Talk pretty_initial_time equal to 09:00' do
-      valid_talks_creator_initialize.call
-
-      expect(Talk.first.pretty_initial_time).to eq('09:00')
-    end
-
-    it 'has second Talk pretty_initial_time not equal to 09:00' do
-      valid_talks_creator_initialize.call
-
-      expect(Talk.second.pretty_initial_time).not_to eq('09:00')
-    end
-
-    it 'counts Talk by 20' do
-      expect{ valid_talks_creator_initialize.call }.to change(Talk, :count).by(20)
+      it 'creates first talk' do
+        expect(Talk.first).to be_truthy
+      end
+  
+      it 'has first talk name equal to first csv line without 60min' do
+        expect(Talk.first.name).to eq('Diminuindo tempo de execução de testes em aplicações Rails enterprise')
+      end
+  
+      it 'has talk initial_time.hour equal to 9' do
+        expect(Talk.first.initial_time.hour).to eq(9)
+      end
+  
+      it 'has first Talk pretty_initial_time equal to 09:00' do
+        expect(Talk.first.pretty_initial_time).to eq('09:00')
+      end
+  
+      it 'has second Talk pretty_initial_time not equal to 09:00' do
+        expect(Talk.second.pretty_initial_time).not_to eq('09:00')
+      end
+  
+      it 'counts Talk by 20' do
+        expect{ valid_talks_creator_initialize.call }.to change(Talk, :count).by(20)
+      end
     end
   end
 end
