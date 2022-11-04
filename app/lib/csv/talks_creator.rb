3ced759e8ddda37c86ca9bffe49a2720cca51f0a
paulo-felipe <alparakleto@gmail.com>
2022-11-04 13:54:01 -0300

    feat liv/csv/talks_creator: create talks whose name is without digit minutes part, save 20 talks  according to 20 lines from the csv

--- app/lib/csv/talks_creator.rb
+++ app/lib/csv/talks_creator.rb
@@ -9,9 +9,17 @@ module Csv
     end
 
     def call
+      talks = []
+
       CSV.read(csv_file).each do |csv|
-        Talk.create(name: csv.first, initial_time: '09:00')
+
+        #TODO: check cases when there is commas in name, must be created with commas
+        name = csv.join.split(/\w+\z/).first.strip!
+        ititial_time = '09:00' if talks.length.zero?
+        talks << Talk.create(name: name, initial_time: ititial_time)
       end
+
+      talks
     end
   end
 end
