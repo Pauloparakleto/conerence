9d47e2f7121105db978773b513eeda7f6ba387a1
paulo-felipe <alparakleto@gmail.com>
2022-11-05 13:15:58 -0300

    chore talk.rb: add belongs to assotiation optional

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,8 @@
 class Talk < ApplicationRecord
   LUNCHING_TIME_HOUR = 12
 
+  belongs_to :track, optional: :true
+
   validate :initial_time_can_not_be_in_lunching_time_hour
   validates_format_of :name, with: /\A([[:punct:][a-zA-Z\u00C0-\u00FF\s]]{1,})\z/
 
